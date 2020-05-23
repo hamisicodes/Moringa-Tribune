4cddc54e1fa9021f94d396954f07004a0623add3
hamisicodes <newhamisi@gmail.com>
2020-05-23 22:43:04 +0300

    change Debug mode to false and edit the allowed host list

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#vvq+ltejyd0ah3hhxdgb8p@k!58cw(*%d6nvl_1_))u+_^rm7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
