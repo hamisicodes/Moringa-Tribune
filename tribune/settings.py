0ecbde121dd90ec8ee7e0a4ac8d96ab9e08ec125
hamisicodes <newhamisi@gmail.com>
2020-05-23 22:46:21 +0300

    changes

--- tribune/settings.py
+++ tribune/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#vvq+ltejyd0ah3hhxdgb8p@k!58cw(*%d6nvl_1_))u+_^rm7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
 
