444e24ad11ba802ce941ae1977e71a24a06f2fe9
hamisicodes <newhamisi@gmail.com>
2020-05-23 22:08:43 +0300

    initial commit

---
+++ Procfile
@@ -0,0 +1,3 @@
+release:python3 manage.py makemigrations news
+release:python3 manage.py migrate
+web: gunicorn tribune.wsgi --log-file -
---
+++ news/tests.py
@@ -0,0 +1,55 @@
+from django.test import TestCase
+from .models import Editor,Article,tags
+import datetime as dt
+
+# Create your tests here.
+
+class EditorTestClass(TestCase):
+    def setUp(self):
+        # creating an new editor and saving it
+        self.james= Editor(firstname = 'James', lastname ='Muriuki', email ='james@moringaschool.com')
+        self.james.save_editor()
+
+        # creating an new tag and saving it
+        self.new_tag = tags(name ='testing')
+        self.new_tag.save()
+
+        # creating an new article and saving it
+        self.new_article = Article(title = 'Test Article' ,post ='This is a random test post' , editor = self.james)
+        self.new_article.save()
+
+        self.new_article.tags.add(self.new_tag)
+
+    def tearDown(self):
+        Editor.objects.all().delete()
+        tags.objects.all().delete()
+        Article.objects.all().delete()
+
+
+    def test_instance(self):
+        self.assertTrue( isinstance(self.james,Editor)  )
+
+    def test_save_method(self):
+        self.james.save_editor()
+        editors = Editor.objects.all()
+
+        self.assertTrue(len(editors) > 0)
+
+    def test_get_news_today(self):
+        today_news = Article.todays_news()
+        self.assertTrue(len(today_news) > 0)
+
+    def test_news_by_date(self):
+        test_date = '2017-03-17'
+        date = dt.datetime.strptime(test_date, '%Y-%m-%d').date()
+        news_by_date = Article.days_news(date)
+
+        self.assertTrue(len(news_by_date) == 0)
+
+
+
+
+
+    
+
+    
